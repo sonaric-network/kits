a839a7b7c402f7e2dd036991dbc9ca8d164f18ee
Alex Nelepa <alexnelepa09@gmail.com>
2024-09-04 14:13:06 +0200

    [chore] small fix start.sh for livepeer

--- livepeer/files/start.sh
+++ livepeer/files/start.sh
@@ -5,4 +5,3 @@
   echo $PASSPHRASE
   echo $PASSPHRASE
 } | livepeer -network mainnet -orchestrator -transcoder -maxSessions 16 -orchSecret asdf -ethUrl https://eth.public-rpc.com -pricePerUnit 800 -serviceAddr $NODE_IP:8935
-sleep 90000000
